3d7ec117e14829bab873ed24e94b4cd0b3d7595a
musa-harb <musa.harb@gmail.com>
2023-03-11 16:45:08 -0800

    list is pulled from firebase and displayed

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
 import 'package:wasteagram/screens/new_post.dart';
 import 'package:wasteagram/widgets/app_scaffold.dart';
 
@@ -21,16 +23,50 @@ class HomePageState extends State<HomePage> {
   Future getImage() async {
     final pickedFile = await picker.pickImage(source: ImageSource.camera);
     image = File(pickedFile!.path);
-    setState(() {});
     return image;
   }
 
   @override
   Widget build(BuildContext context) {
-    return const AppScaffold(
+    return AppScaffold(
         screenName: HomePage.screenName,
-        screen: Center(
-          child: CircularProgressIndicator(),
-        ));
+        screen: StreamBuilder(
+            stream: FirebaseFirestore.instance.collection('posts').snapshots(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Column(
+                  children: [
+                    Expanded(
+                        child: ListView.builder(
+                            itemCount: snapshot.data!.docs.length,
+                            itemBuilder: (context, index) {
+                              var post = snapshot.data!.docs[index];                              
+                              return listTileWidget(
+                                  post['date'], post['quantity']);
+                            }))
+                  ],
+                );
+              } else {
+                return const Center(child: CircularProgressIndicator());
+              }
+            }));
+  }
+
+  Widget listTileWidget(String date, int qty) {
+    return ListTile(
+      leading: Text(formateDate(date)),
+      trailing: Text(
+        qty.toString(),
+      ),
+      dense: true,
+    );
+  }
+
+  String formateDate(String dateTime) {
+    final postDateTime = DateTime.parse(dateTime);
+    final day = DateFormat.EEEE().format(postDateTime).toString();
+    final month = DateFormat.yMMMMd().format(postDateTime).toString();
+    final modifiedDateTime = '$day, $month';
+    return modifiedDateTime;
   }
 }
